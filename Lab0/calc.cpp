86845d510429d394614c5bdbf4c4920684da995c
Beverly <baquon@cpp.edu>
2026-08-27 10:36:16 -0700

    Create calc.cpp

---
+++ Lab0/calc.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file calc.cpp
+ * @author your name (you@domain.com)
+ * @brief data type divison
+ * @version 0.1
+ * @date 2026-08-27
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+ int main() {
+    int int_x = 1;
+    int int_y = 2;
+    double double_z = 2.0;
+
+    cout << "int divided by int: " << int_x / int_y << endl;
+    cout << "int divided by double: " << int_x / double_z << endl;
+
+ }
