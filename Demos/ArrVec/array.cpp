115a91031f1775c1e5db5c06e4c3bd8736bfe2ca
Beverly <baquon@cpp.edu>
2026-09-15 10:40:58 -0700

    typo

--- Demos/ArrVec/array.cpp
+++ Demos/ArrVec/array.cpp
@@ -25,7 +25,7 @@ int main(){
 void printMemArr(const int arr[], int size){
     printf("Array - Each int is worth %lu bytes\n", sizeof(arr[0]));
     for(int i = 0; i < size; i++){
-        printf("Value %i at Memory Locatuib: %p\n", arr[i], arr + i);
+        printf("Value %i at Memory Location: %p\n", arr[i], arr + i);
     }
 }
 
