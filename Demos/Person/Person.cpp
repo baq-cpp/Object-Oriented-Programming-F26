35d19c0bf1150048286d654bd6fe4d82eb1103dd
Beverly <baquon@cpp.edu>
2026-09-24 11:08:50 -0700

    mutators complete

--- Demos/Person/Person.cpp
+++ Demos/Person/Person.cpp
@@ -15,3 +15,22 @@ Person::Person(){
     lives_in_IE = IE;
     //private var = argument
  }
+// updates name to new_name
+ void Person::updateName(string new_name){
+    name = new_name;
+ }
+//update age to new_age
+ void Person::updateAge(int new_age){
+    age = new_age;
+ }
+
+ void Person::updateOccupation(string new_occupation){
+    occupation = new_occupation;
+ }
+
+ //toggle location
+ void Person::moveLocation(){
+    lives_in_IE = ~lives_in_IE;
+ }
+ 
+ string getName() const
