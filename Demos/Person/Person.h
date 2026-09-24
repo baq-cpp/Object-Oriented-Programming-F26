22dfdc49ba63315f1d08ae0e00d56381038681e1
Beverly <baquon@cpp.edu>
2026-09-24 10:59:16 -0700

    added private var

--- Demos/Person/Person.h
+++ Demos/Person/Person.h
@@ -5,22 +5,29 @@
 using namespace std;
 
 class Person{
-    //Constructors + Destructors
-    Person();
-    Person(string name, int age, string occupation, bool lives_in_IE);
-    // ~Person();
+    public:
+        //Constructors + Destructors
+        Person();
+        Person(string name, int age, string occupation, bool lives_in_IE);
+        // ~Person();
 
-    //mutator functions
-    void updateName(string new_name);
-    void updateAge(int new_age);
-    void updateOccupation(string new_occupation);
-    void moveLocation();
+        //mutator functions
+        void updateName(string new_name);
+        void updateAge(int new_age);
+        void updateOccupation(string new_occupation);
+        void moveLocation();
 
-    //accessor functions
-    string getName() const;
-    int getAge() const;
-    string getOccupation() const;
-    bool getLivesInIE() const;
-    bool isOlderThan(Person a) const;
+        //accessor functions
+        string getName() const;
+        int getAge() const;
+        string getOccupation() const;
+        bool getLivesInIE() const;
+        bool isOlderThan(Person a) const;
+
+    private:
+        string name;
+        int age;
+        string occupation;
+        bool lives_in_IE;
 };
 #endif
