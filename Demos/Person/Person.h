52372e28b5f43073a70fd8679064fd6d887c7402
Beverly <baquon@cpp.edu>
2026-09-24 10:52:41 -0700

    added constructors and mutators

--- Demos/Person/Person.h
+++ Demos/Person/Person.h
@@ -1,8 +1,19 @@
 //Person.h
 #ifndef PERSON_H
 #define PERSON_H
+#include <string>
+using namespace std;
 
 class Person{
+    //Constructors + Destructors
+    Person();
+    Person(string name, int age, string occupation, bool lives_in_IE);
+    // ~Person();
 
+    //mutator functions
+    void updateName(string new_name);
+    void updateAge(int new_age);
+    void updateOccupation(string new_occupation);
+    void moveLocation();
 };
 #endif
